5140c1a050d27647d8e91c9379868a8671b9dcf8
Prabhat6969 <coolpkgup.2000@gmail.com>
2020-04-26 23:32:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imageslider/ImageAdapter.java
@@ -0,0 +1,45 @@
+package com.example.imageslider;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImageAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private int[] mImageIds = new int[] {R.drawable.qw,R.drawable.qw1,R.drawable.qw2,R.drawable.qw4};
+
+    ImageAdapter(Context context){
+        mContext = context;
+    }
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(mImageIds[position]);
+        container.addView(imageView,0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ImageView) object);
+    }
+
+}
